f74c556551f1ed92b01eeaed5bd74adf8b360d1b
Michael Joyce <ubermichael@gmail.com>
2022-03-09 10:36:23 -0800

    start the solr search stuff

---
+++ plugins/SolrSearch/tests/phpunit/bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @package     omeka
+ * @subpackage  solr-search
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
+
+define('SOLR_DIR', dirname(dirname(dirname(__FILE__))));
+define('SOLR_TEST_DIR', SOLR_DIR.'/tests/phpunit');
+define('OMEKA_DIR', dirname(dirname(SOLR_DIR)));
+
+// Bootstrap Omeka.
+require_once OMEKA_DIR.'/application/tests/bootstrap.php';
+
+// Base test case.
+require_once 'cases/SolrSearch_Case_Default.php';
+
+// Load other files.
+require_once SOLR_DIR . '/helpers/SolrSearch_Helpers_Index.php';
+require_once SOLR_DIR . '/lib/solr-php-client/Service.php';
